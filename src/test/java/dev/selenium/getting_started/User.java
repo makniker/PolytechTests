d5e92eb7dff82018e26ef7ff51c469348e2a61d0
makniker <ermakov.nd@edu.spbstu.ru>
2022-04-05 14:36:22 +0300

    Added first version

---
+++ src/test/java/dev/selenium/getting_started/User.java
@@ -0,0 +1,28 @@
+package dev.selenium.getting_started;
+
+public class User {
+    private String name_;
+    private String login_;
+    private String password_;
+    User(String name, String login, String password)
+    {
+        name_ = name;
+        login_ = login;
+        password_ = password;
+    }
+
+    public String getName()
+    {
+        return name_;
+    }
+
+    public String getLogin()
+    {
+        return login_;
+    }
+
+    public String getPassword()
+    {
+        return password_;
+    }
+}
